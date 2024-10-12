e58d692d8815094fefa3d0d236b03a1192609757
Flavio Henrique Ferreira Junior <flferrei@c1r4p2.42sp.org.br>
2024-10-12 14:00:07 -0300

    manipulation
    ft_tolower

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,18 +6,18 @@
 /*   By: flferrei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 10:04:24 by flferrei          #+#    #+#             */
-/*   Updated: 2024/10/12 10:09:07 by flferrei         ###   ########.fr       */
+/*   Updated: 2024/10/12 13:49:04 by flferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stddef.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 #endif
