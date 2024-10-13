81b309795b9f8123de236f800438afffe3df16d4
flavio <flavi2101@gmail.com>
2024-10-13 17:19:36 -0300

    ft_strncmp
    update

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,11 +6,10 @@
 /*   By: flferrei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 14:15:23 by flferrei          #+#    #+#             */
-/*   Updated: 2024/10/12 16:58:12 by flferrei         ###   ########.fr       */
+/*   Updated: 2024/10/13 14:48:35 by flaviohenr       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stddef.h>
-#include <assert.h>
+#include "libft.h" 
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -19,13 +18,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	counter = 0;
 	if (n == 0)
 		return (0);
-	while (counter < n && s1[counter] == s2[counter])
+	while (s1[counter] == s2[counter] && (counter <  n)) 
 	{
 		if (s2[counter] == '\0')
 			return (0);
 		++counter;
 	}
 	if (counter == n)
-		return (1);
+		return (0);
 	return ((unsigned char)s1[counter] - (unsigned char)s2[counter]);
 }
