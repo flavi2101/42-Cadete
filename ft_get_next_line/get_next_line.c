8b31a90b83d8fbeeb65446900f220ccee0d78896
Flavio Henrique Ferreira Junior <flferrei@c2r2p6.42sp.org.br>
2024-11-23 15:58:20 -0300

    primeiro commit

--- ft_get_next_line/get_next_line.c
+++ ft_get_next_line/get_next_line.c
@@ -1,7 +1,38 @@
-int	main(void)
-{
-	static int test = 0;
-	test = 1;
-	return (0);
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 
+int	recursive_reading(int fd, int buffer_size, int *real_size)
+{
+	int	size_readed;
+	char	*data;
+
+	data = (char *)malloc(sizeof(char) * buffer_size);
+	if (!data)
+		return (0);
+	size_readed = read(fd, data, buffer_size);
+	while(*real_size <= size_readed)
+	{
+		if (data[*real_size++] == '\n' ||  data[size_readed] == '\0')	
+			return (*real_size - 1);
+	}
+	free(data);
+	recursive_reading(fd, )
+	
+}
+
+char *get_next_line(int fd)
+{
+	static void	*file_buffer;	
+	char		*get_buffer;
+	int			initial_position;
+
+	initial_position = 0;
+	if (fd < 0 || BUFFER == 0)	
+		return (NULL);
+	get_buffer = (char *)malloc(sizeof(char) * BUFFER);
+	if (!get_buffer)
+		return (NULL);
+	recursive_reading(fd, BUFFER, &initial_position);
+	return ((char *)file_buffer);
 }
---
+++ ft_get_next_line/get_next_line.h
@@ -0,0 +1,5 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+int		BUFFER = 42;
+char	*get_next_line(int fd);
+#endif
